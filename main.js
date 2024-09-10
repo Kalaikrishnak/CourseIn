3f5a4124867b851441885af5275c6b6dcec8bbbc
Kalaikrishnak <kalaikrishnak1234@gmail.com>
2024-09-10 15:00:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function showsidebar(){
+    const mobileviewport =document.querySelector('.mobileviewport')
+    mobileviewport.style.display='flex'
+
+
+}
+function hidesidebar(){
+    const mobileviewport =document.querySelector('.mobileviewport')
+    mobileviewport.style.display='none'
+
+
+}
+function showCourse(){
+    const courselist =document.querySelector('.courselist')
+    courselist.style.display="flex"
+
+}
